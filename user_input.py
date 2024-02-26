86baf6c6a19318a94f3cb1335c5e2a894635574c
Danson Ngenya <ngenyadann254@gmail.com>
2024-02-26 14:16:53 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,12 @@
+
+#User's name
+name = input("Enter your name: ")
+#user's age
+age = input("Enter your age: ")
+#user's location
+location = input("Enter your location: ")
+#output
+print('Hello ', name)
+print('You are', age +' years old')
+print('You live in ', location)
+
